ff0d8987e5ccf2b283c84395ff9adcf519d73fa0
VitaUshakov51 <vitavita-98@yandex.ru>
2024-06-21 12:11:08 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 (function () {
 
     Fancybox.bind("[data-fancybox]", {});
+
+
+    const quesions = document.getElementsByClassName('questions__item');
+    for (let i = 0; i < quesions.length; i++) {
+        quesions[i].addEventListener('click', () => {
+            quesions[i].classList.toggle('active');
+        })
+    }
 })()
