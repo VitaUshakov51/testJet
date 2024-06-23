a3b6736e6bd7f1b651b57cc22e58f2e18e5c0966
VitaUshakov51 <vitavita-98@yandex.ru>
2024-06-23 12:17:00 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -9,4 +9,28 @@
             quesions[i].classList.toggle('active');
         })
     }
+
+    const burger = document.getElementById('burger');
+    const overlay = document.getElementById('overlay');
+    const headerNav = document.getElementById('header__nav')
+    burger.addEventListener('click', ()=> {
+        headerNav.classList.toggle('active');
+        burger.classList.toggle('active');
+        overlay.classList.toggle('active');
+    })
+
+    overlay.addEventListener('click', () => {
+        overlay.classList.remove('active');
+        burger.classList.remove('active');
+        headerNav.classList.remove('active');
+    })
+
+    const headerLinks = document.getElementsByClassName('header__nav-link');
+    for (let i = 0; i < headerLinks.length; i++) {
+        headerLinks[i].addEventListener('click', () => {
+            overlay.classList.remove('active');
+            burger.classList.remove('active');
+            headerNav.classList.remove('active');
+        })
+    }
 })()
