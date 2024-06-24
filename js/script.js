e383a1144798ccce51a10d3a3f85bdc69b199d58
VitaUshakov51 <vitavita-98@yandex.ru>
2024-06-24 21:02:02 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -77,4 +77,10 @@
     }
 
 
+    Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
+        get: function () {
+            return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
+        }
+    });
+
 })()
