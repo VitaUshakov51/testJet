c34aae9a6ccc86a78fd42b7dbe19aa24f9b3a221
VitaUshakov51 <vitavita-98@yandex.ru>
2024-06-24 17:09:53 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -4,36 +4,77 @@
 
 
     const quesions = document.getElementsByClassName('questions__item');
-    for (let i = 0; i < quesions.length; i++) {
-        quesions[i].addEventListener('click', () => {
-            quesions[i].classList.toggle('active');
-        })
+    if (quesions) {
+        for (let i = 0; i < quesions.length; i++) {
+            quesions[i].addEventListener('click', () => {
+                quesions[i].classList.toggle('active');
+            })
+        }
     }
+
 
     const burger = document.getElementById('burger');
     const overlay = document.getElementById('overlay');
     const headerNav = document.getElementById('header__nav');
-    burger.addEventListener('click', ()=> {
-        headerNav.classList.toggle('active');
-        burger.classList.toggle('active');
-        overlay.classList.toggle('active');
-    })
 
-    overlay.addEventListener('click', () => {
-        overlay.classList.remove('active');
-        burger.classList.remove('active');
-        headerNav.classList.remove('active');
-    })
+    if (burger && overlay && headerNav ) {
+        burger.addEventListener('click', ()=> {
+            headerNav.classList.toggle('active');
+            burger.classList.toggle('active');
+            overlay.classList.toggle('active');
+        })
 
-    const headerLinks = document.getElementsByClassName('header__nav-link');
-    for (let i = 0; i < headerLinks.length; i++) {
-        headerLinks[i].addEventListener('click', () => {
+        overlay.addEventListener('click', () => {
             overlay.classList.remove('active');
             burger.classList.remove('active');
             headerNav.classList.remove('active');
         })
     }
 
+
+
+
+    const headerLinks = document.getElementsByClassName('header__nav-link');
+    if (headerLinks) {
+        for (let i = 0; i < headerLinks.length; i++) {
+            headerLinks[i].addEventListener('click', () => {
+                overlay.classList.remove('active');
+                burger.classList.remove('active');
+                headerNav.classList.remove('active');
+            })
+        }
+    }
+
+
+    const phone = document.getElementById('phone');
+    const maskOptions = {
+        mask: '+{7}(000)000-00-00'
+    };
+    const mask = IMask(phone, maskOptions);
+
+    const formButton = document.getElementById('formButton');
+    if (formButton) {
+        formButton.addEventListener('click', () => {
+            const form = $(this).closest('form');
+            const actUrl = form.attr('action');
+            $.ajax({
+                url: actUrl,
+                type: 'post',
+                dataType: 'html',
+                data: form.serialize(),
+                success: function (data) {
+                    sessionStorage.setItem('formButton', 'send');
+                    console.log('Форма отправлена')
+                },
+                error: function () {
+
+
+                }
+            });
+
+        })
+
+    }
 
 
 })()
